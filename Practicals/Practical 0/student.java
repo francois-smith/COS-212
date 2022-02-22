48d90c216986f91ababae69736b44eeb1e926b37
francois-smith <steamfaffa@gmail.com>
2022-02-22 20:18:52 +0200

    Deleted Test File and Uploaded Prac 0

---
+++ Practicals/Practical 0/student.java
@@ -0,0 +1,5 @@
+public class student {
+    public void sayHello(){
+        System.out.println("I u21649988 am a COS212 student. I pinky promise that I will work hard in this module and not cheat!!!");          
+    }
+}
