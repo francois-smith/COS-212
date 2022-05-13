0bf4f650fefdffd771d3370f53c72928e5836dec
Francois Smith <steamfaffa@gmail.com>
2022-05-13 11:29:20 +0200

    Started Insert implementation

--- Assignments/Assignment 3/BPTreeLeafNode.java
+++ Assignments/Assignment 3/BPTreeLeafNode.java
@@ -32,6 +32,42 @@ class BPTreeLeafNode<TKey extends Comparable<TKey>, TValue> extends BPTreeNode<T
 
 	////// You should not change any code above this line //////
 
-	////// Implement functions below this line //////
+	public BPTreeNode<TKey, TValue> insert(TKey key, TValue value){
+		int keyCount = this.getKeyCount();
 
+		if(keyCount < m){ //leaf has space for insert
+			if(keyCount == 0){ //the leaf is empty
+				this.setKey(0, key);
+				this.setValue(0, value);
+				this.keyTally++;
+				return this;
+			}
+
+			for(int index = 0; index < keyCount; index++){
+				if(this.getKey(index).compareTo(key) > 0){
+					for(int moveIndex){
+
+					}
+
+					this.setKey(index, key);
+					this.setValue(index, value);
+					this.keyTally++;
+					break;
+				}
+			}
+		}
+		return this;
+	}
+
+	////// Implement functions below this line //////
+	// public BPTreeNode<TKey, TValue> delete(TKey key){
+	// 	for(int i = 0; i < this.getKeyCount(); i++){
+	// 		if(this.getKey(i).equals(key)){
+	// 			for(j = i; j <this )
+	// 		}
+	// 	}
+
+	// 	return this;
+	// }
+	
 }
