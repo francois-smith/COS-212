56b82e750e3b554ac0a4fd9393c43dc4fc63be70
Francois Smith <steamfaffa@gmail.com>
2022-05-16 15:16:35 +0200

    Fixed Bubble Sort

--- Practicals/Practical 8/BubbleSort.java
+++ Practicals/Practical 8/BubbleSort.java
@@ -5,30 +5,27 @@ public class BubbleSort extends Sorting {
 
     @Override
     public String[] sortAcs(Vertex[] array){        
-        for(int i = 0; i < array.length - 1; i++){
-            for(int j = 0; j < array.length - i - 1; j++){
-                if(array[j].getVName().compareTo(array[j+1].getVName()) > 0){
+        for(int i = 0; i <= array.length - 2; i++){
+            for(int j = array.length-1; j >= i+1; j--){
+                if(array[j].getVName().compareTo(array[j-1].getVName()) < 0){
                     Vertex temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
                 }
-                
             }
             printArr(vertexToString(array));
         }
         return vertexToString(array);
     }
-
     @Override
     public String[] sortDsc(Vertex[] array) {
-        for(int i = 0; i < array.length - 1; i++){
-            for(int j = 0; j < array.length - i - 1; j++){
-                if(array[j].getVName().compareTo(array[j+1].getVName()) < 0){
+        for(int i = 0; i <= array.length - 2; i++){
+            for(int j = array.length-1; j >= i+1; j--){
+                if(array[j].getVName().compareTo(array[j-1].getVName()) > 0){
                     Vertex temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j-1];
+                    array[j-1] = temp;
                 }
-                
             }
             printArr(vertexToString(array));
         }
