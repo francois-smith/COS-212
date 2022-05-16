0acc7b01547e138a8999e612920d0964a3b0379c
Francois Smith <steamfaffa@gmail.com>
2022-05-16 15:16:26 +0200

    Finished Topological Sort

--- Practicals/Practical 8/TopologicalSort.java
+++ Practicals/Practical 8/TopologicalSort.java
@@ -1,19 +1,103 @@
 public class TopologicalSort extends Sorting {
+    Integer i = 0, j = 0;
+
     public TopologicalSort(){
+        this.name = "TopologicalSort";
     }
 
     @Override
     public String[] sortAcs(Vertex[] array) throws CycleException {
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        try{
+            for(int i = 0; i < array.length; i++){
+                array[i].setNum(0);
+                array[i].setTSNum(0);
+            }
+    
+            i = 1;
+            j = array.length;
+    
+            for(int i = 0; i < array.length; i++){
+                if(array[i].getTSNum() == 0){
+                    tsSort(array[i]);
+                }
+            }
+
+            for(int i = 0; i < array.length - 1; i++){
+                for(int j = 0; j < array.length - i - 1; j++){
+                    if(array[j].getTSNum() > array[j+1].getTSNum()){
+                        Vertex temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+    
+            return vertexToString(array);
+        }
+        catch(Exception e){
+            throw e;
+        }
     }
 
     @Override
     public String[] sortDsc(Vertex[] array) throws CycleException{
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        try{
+            for(int i = 0; i < array.length; i++){
+                array[i].setNum(0);
+                array[i].setTSNum(0);
+            }
+    
+            i = 1;
+            j = array.length;
+    
+            for(int i = 0; i < array.length; i++){
+                if(array[i].getTSNum() == 0){
+                    tsSort(array[i]);
+                }
+            }
+
+            for(int i = 0; i < array.length - 1; i++){
+                for(int j = 0; j < array.length - i - 1; j++){
+                    if(array[j].getTSNum() < array[j+1].getTSNum()){
+                        Vertex temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+    
+            return vertexToString(array);
+        }
+        catch(Exception e){
+            throw e;
+        }
     }
 
+    private void tsSort(Vertex v) throws CycleException{
+        v.setNum(i++);
+
+        Edge[] edges = v.getEdges();
+        for(int k = 0; k < edges.length; k++){
+            Edge edge = edges[k];
+            Vertex adjacent = getVertex(edge, v);
+            if(adjacent.getNum() == 0){
+                tsSort(adjacent);
+            }
+            else if(adjacent.getTSNum() == 0){
+                throw new CycleException();
+            }
+        }
+        v.setTSNum(j--);
+    }
+
+    private Vertex getVertex(Edge edge, Vertex v){
+        if(edge.pointA.equals(v)){
+            return edge.pointB;
+        }
+        else{
+            return edge.pointA;
+        }
+    }
 }
 
 class CycleException extends Exception{
