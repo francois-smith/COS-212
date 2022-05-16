e7c13882c7e4e1474b25d3e34500340fbcc4fe33
Francois Smith <steamfaffa@gmail.com>
2022-05-16 15:16:54 +0200

    Finished Selection Sort

--- Practicals/Practical 8/SelectionSort.java
+++ Practicals/Practical 8/SelectionSort.java
@@ -1,17 +1,42 @@
 public class SelectionSort extends Sorting {
     
     public SelectionSort(){
+        this.name = "SelectionSort";
     }
 
     @Override
     public String[] sortAcs(Vertex[] array) {
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        for(int i = 0; i <= array.length - 1; i++){
+            int id = i;
+            for(int j = i; j < array.length; j++){
+                if(array[j].getVName().compareTo(array[id].getVName()) < 0){
+                    id = j;
+                }
+            }
+
+            Vertex temp = array[id];
+            array[id] = array[i];
+            array[i] = temp;
+            printArr(vertexToString(array));
+        }
+        return vertexToString(array);
     }
 
     @Override
     public String[] sortDsc(Vertex[] array) {
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        for(int i = 0; i <= array.length - 1; i++){
+            int id = i;
+            for(int j = i; j < array.length; j++){
+                if(array[j].getVName().compareTo(array[id].getVName()) > 0){
+                    id = j;
+                }
+            }
+
+            Vertex temp = array[id];
+            array[id] = array[i];
+            array[i] = temp;
+            printArr(vertexToString(array));
+        }
+        return vertexToString(array);
     }
 }
