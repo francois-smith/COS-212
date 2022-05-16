54642a4249f1f496265faeb6bffc0d5a7a0ae81f
Francois Smith <steamfaffa@gmail.com>
2022-05-16 15:16:44 +0200

    Finished Comb Sort

--- Practicals/Practical 8/CombSort.java
+++ Practicals/Practical 8/CombSort.java
@@ -1,16 +1,57 @@
 public class CombSort extends Sorting {
     public CombSort(){
+        this.name = "CombSort";
     }
 
     @Override
     public String[] sortAcs(Vertex[] array) {
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        int gap = array.length;
+        Boolean swapped = true;
+
+        while(gap > 1 || swapped){
+            swapped = false;
+            if(gap > 1){
+                gap = (gap*10)/13;
+            }
+
+            for(int i=0; i < array.length-gap; i++){
+                if(array[i].getVName().compareTo(array[i+gap].getVName()) > 0){
+                    Vertex temp = array[i];
+                    array[i] = array[i+gap];
+                    array[i+gap] = temp;
+                    
+                    swapped = true;
+                }
+            }
+            printArr(vertexToString(array));
+            System.out.print("Gap: "+gap+"\n");
+        }
+        return vertexToString(array);
     }
 
     @Override
     public String[] sortDsc(Vertex[] array) {
-        String[] returnArray = new String[]{"Dwadwad"};
-        return returnArray;
+        int gap = array.length;
+        Boolean swapped = true;
+
+        while(gap > 1 || swapped){
+            swapped = false;
+            if(gap > 1){
+                gap = (gap*10)/13;
+            }
+
+            for(int i=0; i < array.length-gap; i++){
+                if(array[i].getVName().compareTo(array[i+gap].getVName()) < 0){
+                    Vertex temp = array[i];
+                    array[i] = array[i+gap];
+                    array[i+gap] = temp;
+                    
+                    swapped = true;
+                }
+            }
+            printArr(vertexToString(array));
+            System.out.print("Gap: "+gap+"\n");
+        }
+        return vertexToString(array);
     }
 }
