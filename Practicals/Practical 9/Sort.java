ae5a6d89307ca433fd750e1c7b143b15d7cada4e
Francois Smith <steamfaffa@gmail.com>
2022-06-02 16:34:35 +0200

    Finished CountingSort

--- Practicals/Practical 9/Sort.java
+++ Practicals/Practical 9/Sort.java
@@ -7,28 +7,51 @@ public class Sort
 	////// Implement the functions below this line //////
 	
 	/********** MERGE **********/
-	public static <T extends Comparable<? super T>> void mergesort(T[] data, int first, int last, boolean debug)
-	{
-
-		// Your code here
+	public static <T extends Comparable<? super T>> void mergesort(T[] data, int first, int last, boolean debug){
 
 	}
      
-	private static <T extends Comparable<? super T>> void merge(T[] data, int first, int last, boolean debug)
-	{
-
-		// Your code here
+	private static <T extends Comparable<? super T>> void merge(T[] data, int first, int last, boolean debug){
         
+
 		//DO NOT MOVE OR REMOVE!
 		if (debug)
 			System.out.println(Arrays.toString(data));
 	}
      
 	/********** COUNTING **********/
-	public static void countingsort(int[] data, boolean debug)
-	{
+	public static void countingsort(int[] data, boolean debug){
+		int[] temp = new int[data.length];
+		int largest = data[0];
+		int index;
 
-		// Your code here
+		for(index = 1; index < data.length; index++){
+			if(largest < data[index]){
+				largest = data[index];
+			}
+		}
+
+		int[] count = new int[largest+1];
+		for(index = 0; index < largest; index++){
+			count[index] = 0;
+		}
+
+		for(index = 0; index < data.length; index++){
+			count[data[index]]++;
+		}
+
+		for(index = 1; index <= largest; index++){
+			count[index] = count[index-1] + count[index];
+		}
+
+		for(index = data.length-1; index >= 0; index--){
+			temp[count[data[index]]-1] = data[index];
+			count[data[index]]--;
+		}
+
+		for(index = 0; index < data.length; index++){
+			data[index] = temp[index];
+		}
 
 		//DO NOT MOVE OR REMOVE!
 		if (debug)
