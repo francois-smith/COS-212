186d4d5b5a810319d7027ec557c9f610d5e93a00
Francois Smith <steamfaffa@gmail.com>
2022-06-02 17:27:30 +0200

    Practical 9 Full Marks

--- Practicals/Practical 9/Sort.java
+++ Practicals/Practical 9/Sort.java
@@ -5,14 +5,50 @@ public class Sort
 {
 	
 	////// Implement the functions below this line //////
-	
 	/********** MERGE **********/
 	public static <T extends Comparable<? super T>> void mergesort(T[] data, int first, int last, boolean debug){
-
+		if ((last - first) < 1){
+			return;
+		} 
+		
+		int middle = (first+last)/2;
+		mergesort(data, first, middle, debug);
+		mergesort(data, middle + 1, last, debug);
+		merge(data, first, last, debug);
 	}
      
+	@SuppressWarnings("unchecked")
 	private static <T extends Comparable<? super T>> void merge(T[] data, int first, int last, boolean debug){
-        
+		Object[] tmpArr = new Object[last-first+1]; 
+		int middle = (first+last)/2;
+		int index1 = first;
+		int index2 = middle+1;
+		int index3 = 0;
+
+		while(index1 <= middle && index2 <= last){
+			if(data[index1].compareTo(data[index2]) <= 0){
+				tmpArr[index3] = data[index1++];
+			}
+			else{
+				tmpArr[index3] = data[index2++];
+			}
+
+			index3++;
+		}
+		if(index1 <= middle && index2 > last){
+			while (index1 <= middle){
+				tmpArr[index3++] = data[index1++];
+			}
+		} 
+		else{
+			while (index2 <= last){
+				tmpArr[index3++] = data[index2++];
+			}
+		}
+
+		for(index3 = 0; index3 < tmpArr.length; index3++){
+			data[index3+first] = (T)(tmpArr[index3]);
+		}
 
 		//DO NOT MOVE OR REMOVE!
 		if (debug)
